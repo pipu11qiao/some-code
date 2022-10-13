8909fdd287217055d03801e0585826adc21f59c9
王勇 <wangyong16@xiaomi.com>
2022-10-13 19:53:16 +0800

    feat: update

--- js/express/path-to-regexp-0.1.7/fileTest/test_copy.js
+++ js/express/path-to-regexp-0.1.7/fileTest/test_copy.js
@@ -4,11 +4,24 @@ describe('path-to-regexp-strings', function () {
   it('should match simple paths', function () {
     const parmas = [];
     const m = pathToRegExp('/test', parmas).exec('/test');
-    console.log(`m`, m);
 
     assert.equal(parmas.length, 0);
 
     assert.equal(m.length, 1);
     assert.equal(m[0], '/test');
   })
+  it('should match express format params', function () {
+
+    const parmas = [];
+    const m = pathToRegExp('/:test', parmas).exec('/pathname');
+
+    assert.equal(parmas.length, 1);
+    assert.equal(parmas[0].name, 'test');
+    assert.equal(parmas[0].optional, false);
+
+    assert.equal(m.length, 2);
+    assert.equal(m[0], '/pathname');
+    assert.equal(m[1], 'pathname');
+  })
+
 })
